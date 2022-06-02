90fc7bdb9c207bffc61c1417bda7492091505c7a
realsonya7 <realsonya7@gmail.com>
2022-06-02 23:11:14 +0900

    서버 베이스변경3 서버 베이스변경3

--- Server/index.js
+++ Server/index.js
@@ -27,9 +27,6 @@ app.post('/signup', controllers.signup);
 
 const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
 
-// 인증서 파일들이 존재하는 경우에만 https 프로토콜을 사용하는 서버를 실행합니다.
-// 만약 인증서 파일이 존재하지 않는경우, http 프로토콜을 사용하는 서버를 실행합니다.
-// 파일 존재여부를 확인하는 폴더는 서버 폴더의 package.json이 위치한 곳입니다.
 let server;
 if (fs.existsSync('./key.pem') && fs.existsSync('./cert.pem')) {
   const privateKey = fs.readFileSync(__dirname + '/key.pem', 'utf8');
