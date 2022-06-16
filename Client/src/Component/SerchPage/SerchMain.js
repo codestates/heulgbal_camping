09821b238451a3492d556a6ee0555a0976936bad
plutio1592 <luke159295@gmail.com>
2022-06-16 23:06:41 +0900

    예약완료 화면 제작

--- Client/src/Component/SerchPage/SerchMain.js
+++ Client/src/Component/SerchPage/SerchMain.js
@@ -3,35 +3,28 @@ import React from 'react';
 import './SerchMain.css'
 
 const Main = () => {
+
+  const mainClick = (e) => {
+    window.location.href = "/"
+  }
+
   return (
   <div>
     <div className='serchmain-border'>
-        <button className='serchmain-main-button serchmain-text serchmain-button'>흙발캠핑</button>
+        <button className='serchmain-main-button serchmain-text serchmain-button' onClick={mainClick}>흙발캠핑</button>
         <button className='serchmain-serch-button serchmain-text serchmain-button' >검색</button>
         <button className='serchmain-profile-button serchmain-text serchmain-button' >프로필</button>
       </div>
       <div className='serchmain-mainbox'>
-    <p className='serchmain-sidebar'>
-    <button className='serchmain-sidebutton serchmain-button'>회원정보</button>
-    <button className='serchmain-sidebutton serchmain-button'>캠핑장 예약관리</button>
-    <button className='serchmain-sidebutton serchmain-button'>회원탈퇴</button>
-    </p>
+    <span className='serchmain-sidebar'>
+    <button className='serchmain-sidebutton serchmain-button'>날짜</button>
+    <button className='serchmain-sidebutton serchmain-button'>지역</button>
+    <button className='serchmain-sidebutton serchmain-button'>캠핑장 이름</button>
+    <button className='serchmain-sidebutton serchmain-button'>인원설정</button>
+    <button className='serchmain-sidebutton serchmain-button'>숙소 검색</button>
+    </span>
     <div className='serchmain-page-input'>
-      <div className='serchmain-input-text'>
-        이름
-      </div>
-      <input className='input-box'/>
-      <div className='serchmain-input-text'>
-      연락처
-      </div>
-      <input className='input-box'/>
-      <div className='serchmain-input-text'>
-        이메일
-      </div>
-      <input className='input-box'/>
-      <div />
-    <button className='serchmain-text password-button serchmain-button'>비밀번호 변경</button>
-    <button className='serchmain-text serchmainpage-edit-button serchmain-button'>개인정보 수정</button>
+      
     </div>
       </div>
     </div>
